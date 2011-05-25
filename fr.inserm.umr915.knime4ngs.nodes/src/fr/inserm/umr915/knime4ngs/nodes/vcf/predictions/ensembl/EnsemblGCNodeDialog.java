1a29a96e195ee5fff5d43526270828971cf3b031
plindenbaum.u915 <plindenbaum.u915@d931a406-db19-d2ed-a82e-cc0d0b040b69>
2011-05-25 11:15:41 +0000

    prediction + bam

--- fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/predictions/ensembl/EnsemblGCNodeDialog.java
+++ fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/predictions/ensembl/EnsemblGCNodeDialog.java
@@ -1,13 +1,24 @@
 package fr.inserm.umr915.knime4ngs.nodes.vcf.predictions.ensembl;
 
+import org.knime.core.data.def.StringCell;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentStringListSelection;
+import org.knime.core.node.defaultnodesettings.SettingsModelColumnName;
 import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
+
+import fr.inserm.umr915.knime4ngs.corelib.knime.DataTypeColumnFilter;
 
 public class EnsemblGCNodeDialog extends DefaultNodeSettingsPane
 	{
     protected EnsemblGCNodeDialog()
     	{
+    	//columns
+    	addDialogComponent(new DialogComponentColumnNameSelection(
+    			new SettingsModelColumnName(EnsemblGCNodeModel.PROPERTY_INFO_COL, EnsemblGCNodeModel.DEFAULT_INFO_COL),
+    			"INFO", 0, true, false,
+    			new DataTypeColumnFilter(StringCell.TYPE)));
+    	
     	SettingsModelStringArray stringModel=new SettingsModelStringArray( EnsemblGCNodeModel.GC_PROPERTY, EnsemblGCNodeModel.DEFAULT_CHOICE);
         addDialogComponent(new DialogComponentStringListSelection(stringModel, "Prediction", EnsemblGCNodeModel.CHOICE));
     	}

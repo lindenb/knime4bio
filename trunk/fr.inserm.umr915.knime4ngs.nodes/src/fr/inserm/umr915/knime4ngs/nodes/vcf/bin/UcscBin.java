8d0dbe5f82b822dbeceb894ff165da1b0860ab05
plindenbaum.u915 <plindenbaum.u915@d931a406-db19-d2ed-a82e-cc0d0b040b69>
2011-06-08 15:14:28 +0000

    cont

--- trunk/fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/bin/UcscBin.java
+++ trunk/fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/bin/UcscBin.java
@@ -1,5 +1,8 @@
 package fr.inserm.umr915.knime4ngs.nodes.vcf.bin;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class UcscBin
 	{
 	private static int calcBin(
@@ -54,6 +57,53 @@ public class UcscBin
 			}
 
 	
+	  private static Set<Integer> collectBins(
+              final int chromStart,
+              final int chromEnd,
+              int binId,
+              int level,
+              int binRowStart,
+              int rowIndex,
+              int binRowCount,
+              int genomicPos,
+              int genomicLength,
+              Set<Integer> set
+              )
+              {
+              set.add(binId);
+              if(level>=MaxLevel) return set;
+              
+              int childLength=genomicLength/childrenCount;
+              int childBinRowCount=binRowCount*childrenCount;
+              int childRowBinStart=binRowStart+binRowCount;
+              int firstChildIndex=rowIndex*childrenCount;
+              int firstChildBin=childRowBinStart+firstChildIndex;
+              for(int i=0;i< childrenCount;++i)
+                      {
+                      int childStart=genomicPos+i*childLength;
+                      
+                      if( chromStart>(childStart+childLength) ||
+                              chromEnd<childStart )
+                              {
+                              continue;
+                              }
+                      collectBins(
+                              chromStart,
+                              chromEnd,
+                              firstChildBin+i,
+                              level+1,
+                              childRowBinStart,
+                              firstChildIndex+i,
+                              childBinRowCount,
+                              childStart,
+                              childLength,
+                              set
+                              );
+                      }
+              return set;
+              }
+
+	
     private static final int childrenCount=8;
 	private static final int MaxLevel=4;
 	private static final int MaxGenomicLengthLevel=536870912;//2^29
@@ -68,5 +118,12 @@ public class UcscBin
 		{
 		return calcBin(chromStart,chromEnd,0,0,0,0,1,0,MaxGenomicLengthLevel);
 		}
-	
+	 /** returns all the bin-ids overlapping the given range */
+    public static Integer[] getBins(int chromStart,int chromEnd)
+            {
+            int genomicLength=MaxGenomicLengthLevel;
+            Set<Integer> set= collectBins(chromStart,chromEnd,0,0,0,0,1,0,genomicLength,new TreeSet<Integer>());
+            return set.toArray(new Integer[set.size()]);
+            }
+
 	}
--- trunk/fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/ucsc/generic/CodeGenerator.java
+++ trunk/fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/ucsc/generic/CodeGenerator.java
@@ -21,6 +21,7 @@ private static class  KTable
 	String databaseName;
 	String uri;
 	SQLSchemaParser.Table table;
+	int bin=-1;
 	int chrom;
 	int chromStart;
 	int chromEnd;
@@ -28,7 +29,7 @@ private static class  KTable
 		{
 		return databaseName+"."+table.name;
 		}
-	private void echo(PrintStream out) throws Exception
+	private void echoText(PrintStream out) throws Exception
 		{
 		out.println("new UcscDatabaseHandler()");
 		out.println("	{");
@@ -106,6 +107,103 @@ private static class  KTable
 		out.println("		}");
 		out.println("	}");
 		}
+	
+	private void echoSql(PrintStream out) throws Exception
+		{
+		out.println("new UcscDatabaseMysqlHandler()");
+		out.println("	{");
+		out.println("public int getBinColumn() { return "+this.bin+";}");
+		out.println("public int getChromColumn() { return "+this.chrom+";}");
+		out.println("public int getChromStartColumn() { return "+this.chromStart+";}");
+		out.println("public int getChromEndColumn() { return "+this.chromEnd+";}");
+		StringBuilder b=new StringBuilder("select ");
+		for(int i=0;i< table.columns.size();++i)
+			{
+			SQLSchemaParser.Column col=table.columns.get(i);
+			if(i>0) b.append(",");
+			b.append(col.name);
+			}
+		b.append(" from ").
+			append(databaseName).
+			append(".").
+			append(this.table.name).
+			append(" where ").
+			append(table.get(this.chrom).name).
+			append("=? and not(").
+			append(table.get(this.chromEnd).name).append("<=? or ").
+			append(table.get(this.chromStart).name).append(">?").
+			append(")");
+	
+		if(this.bin!=-1)
+			{
+			b.append(" and ").append(table.get(this.bin).name).append("=?");
+			}
+		
+		
+		out.println("public String getPreparedStatement() { return \""+b.toString()+"\";}");
+		out.println("	public DataTableSpec getDataTableSpec()");
+		out.println("		{");
+		out.println("		DataColumnSpec  colSpecs[]=new DataColumnSpec["+table.columns.size()+"];");
+		for(int i=0;i< table.columns.size();++i)
+			{
+			String datatType="StringCell.TYPE";
+			if( table.columns.get(i).type==Long.class)
+				{
+				datatType="LongCell.TYPE";
+				}
+			else if( table.columns.get(i).type==Double.class)
+				{
+				datatType="DoubleCell.TYPE";
+				}
+			else if( table.columns.get(i).type==Integer.class)
+				{
+				datatType="IntCell.TYPE";
+				}
+			out.println("		colSpecs["+i+"]=new DataColumnSpecCreator(getDatabaseName()+\".\"+getTableName()+\"."+table.columns.get(i).name+"\","+datatType+").createSpec();");
+			}
+		out.println("		return new DataTableSpec(colSpecs);");
+		out.println("		}");
+		out.println("	public String getDatabaseName()");
+		out.println("		{");
+		out.println("		return \""+databaseName+"\";");
+		out.println("		}");
+		out.println("	public String getTableName()");
+		out.println("		{");
+		out.println("		return \""+table.name+"\";");
+		out.println("		}");
+		out.println("	public DataCell[] parse(ResultSet row) throws SQLException");
+		out.println("		{");
+		out.println("DataCell cells[]=new DataCell["+table.columns.size()+"];");
+		for(int i=0;i< table.columns.size();++i)
+			{
+			SQLSchemaParser.Column col=table.columns.get(i);
+			
+			if( col.type==Long.class)
+				{
+				out.println("			cells["+i+"]=new LongCell(row.getLong(\""+ col.name+"\"));");
+				out.println("			if(row.wasNull()) cells["+i+"]=DataType.getMissingCell();");
+				}
+			else if( col.type==Double.class)
+				{
+				out.println("			cells["+i+"]=new DoubleCell(row.getDouble(\""+ col.name+"\"));");
+				out.println("			if(row.wasNull()) cells["+i+"]=DataType.getMissingCell();");
+				}
+			else if( col.type==Integer.class)
+				{
+				out.println("			cells["+i+"]=new IntCell(row.getInt(\""+ col.name+"\"));");
+				out.println("			if(row.wasNull()) cells["+i+"]=DataType.getMissingCell();");
+				}
+			else
+				{
+				out.println("			cells["+i+"]=new StringCell(row.getString(\""+ col.name+"\"));");
+				out.println("			if(row.wasNull()) cells["+i+"]=DataType.getMissingCell();");
+				}
+			}
+		out.println("		return cells;");
+		out.println("		}");
+		out.println("	}");
+		}	
+	
 	}
 
 private List<KTable> ktables=new ArrayList<CodeGenerator.KTable>();
@@ -143,7 +241,7 @@ public void run(
 		InputStream in=url2.openStream();
 		ktable.table=new SQLSchemaParser(in).input();
 		in.close();
-		
+		ktable.bin= ktable.table.findColumnIndex("bin");
 		ktable.chrom= ktable.table.findColumnIndex("chrom");
 		if(ktable.chrom==-1 || ktable.table.get(ktable.chrom).type!=String.class) continue;
 		ktable.chromStart= ktable.table.findColumnIndex("chromStart");
@@ -159,17 +257,25 @@ public void run(
 	
 	}
 
-private void run(PrintStream out) throws Exception
+private void run(PrintStream out,String type) throws Exception
 	{
 	run("hg19","http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/");
-	echo(out);
+	if(type.equals("sql"))
+		{
+		echoSqlHandler(out);
+		}
+	else
+		{
+		echoTextHandler(out);
+		}
 	}
 
-private void echo(PrintStream out) throws Exception
+private void echoTextHandler(PrintStream out) throws Exception
 	{
 	out.println("package fr.inserm.umr915.knime4ngs.nodes.vcf.ucsc.generic;");
 	out.println("import org.knime.core.data.*;");
 	out.println("import org.knime.core.data.def.*;");
+	
 	
 	out.println("public class UcscTableHandlers");
 	out.println("	{");
@@ -189,7 +295,7 @@ private void echo(PrintStream out) throws Exception
 		{
 		KTable t=this.ktables.get(i);
 		out.println("if(id.equals(\""+t.getId()+"\")) return ");
-		t.echo(out);
+		t.echoText(out);
 		out.println(";");
 		}
 	out.println("		return null;");
@@ -197,9 +303,45 @@ private void echo(PrintStream out) throws Exception
 	out.println("	}");
 	}
 
-public static void main(String[] args) throws Exception
+private void echoSqlHandler(PrintStream out) throws Exception
 	{
+	out.println("package fr.inserm.umr915.knime4ngs.nodes.vcf.ucsc.generic;");
+	out.println("import org.knime.core.data.*;");
+	out.println("import org.knime.core.data.def.*;");
+	out.println("import java.sql.*;");
+	//out.println("import fr.inserm.umr915.knime4ngs.nodes.vcf.bin.UcscBin;");
 	
+	
+	out.println("public class UcscTableMysqlHandlers");
+	out.println("	{");
+	out.println("	private static String TABLE_IDS[]=new String[]{");
+	for(int i=0;i< this.ktables.size();++i)
+		{
+		KTable t=this.ktables.get(i);
+		if(i>0) out.print(",");
+		out.println("\""+t.getId()+"\"");
+		}
+	out.println("		};");
+	out.println("	public UcscTableMysqlHandlers() {} ");
+	out.println("	public static String[] getTableIds() { return TABLE_IDS;}");
+	out.println("	public UcscDatabaseMysqlHandler getHandlerById(String id)");
+	out.println("		{");
+	for(int i=0;i< this.ktables.size();++i)
+		{
+		KTable t=this.ktables.get(i);
+		out.println("if(id.equals(\""+t.getId()+"\")) return ");
+		t.echoSql(out);
+		out.println(";");
+		}
+	out.println("		return null;");
+	out.println("		}");
+	out.println("	}");
+	}
+
+
+public static void main(String[] args) throws Exception
+		{
+		String type="text";
 			File fileOut=null;
 			int optind=0;
 			while(optind< args.length)
@@ -211,12 +353,17 @@ public static void main(String[] args) throws Exception
 					System.err.println("Author:Pierre Lindenbaum PhD.");
 					System.err.println("Options:");
 					System.err.println(" -h help; This screen.");
+					System.err.println(" -t <type> (text or sql)");
 					System.err.println(" -o <file> outputfile");
 					return;
 					}
 				else if(args[optind].equals("-o"))
 					{
 					fileOut=new File(args[++optind]);
+					}
+				else if(args[optind].equals("-t") || args[optind].equals("-type"))
+					{
+					type=args[++optind];
 					}
 				else if(args[optind].equals("--"))
 					{
@@ -244,7 +391,7 @@ public static void main(String[] args) throws Exception
 		{
 		out=new PrintStream(fileOut);
 		}
-	new CodeGenerator().run(out);
+	new CodeGenerator().run(out,type);
 	out.flush();
 	if(fileOut!=null)
 		{

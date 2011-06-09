f134b28f2e8e72db9bb3e01bbdc4e900839a287b
plindenbaum.u915 <plindenbaum.u915@d931a406-db19-d2ed-a82e-cc0d0b040b69>
2011-06-09 14:43:40 +0000

    nsfp

--- fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/nsfp/NsfpNodeDialog.java
+++ fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/nsfp/NsfpNodeDialog.java
@@ -2,9 +2,15 @@ package fr.inserm.umr915.knime4ngs.nodes.vcf.nsfp;
 
 import javax.swing.JFileChooser;
 
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.StringCell;
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.SettingsModelColumnName;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+import fr.inserm.umr915.knime4ngs.corelib.knime.DataTypeColumnFilter;
 
 
 public class NsfpNodeDialog extends DefaultNodeSettingsPane
@@ -18,7 +24,27 @@ public class NsfpNodeDialog extends DefaultNodeSettingsPane
     		false,
     		".zip",".ZIP"
     		);
+    	
     	addDialogComponent(fileSel);
+    	
+    	//columns
+    	addDialogComponent(new DialogComponentColumnNameSelection(
+    			new SettingsModelColumnName(NsfpNodeModel.PROPERTY_CHROM_COL, NsfpNodeModel.DEFAULT_CHROM_COL),
+    			"Chrom", 0, true, false,
+    			new DataTypeColumnFilter(StringCell.TYPE)));
+    	addDialogComponent(new DialogComponentColumnNameSelection(
+    			new SettingsModelColumnName(NsfpNodeModel.PROPERTY_POS_COL, NsfpNodeModel.DEFAULT_POS_COL),
+    			"POS", 0, true, false,
+    			new DataTypeColumnFilter(IntCell.TYPE)));
+    	addDialogComponent(new DialogComponentColumnNameSelection(
+    			new SettingsModelColumnName(NsfpNodeModel.PROPERTY_REF_COL, NsfpNodeModel.DEFAULT_REF_COL),
+    			"REF", 0, true, false,
+    			new DataTypeColumnFilter(StringCell.TYPE)));
+    	addDialogComponent(new DialogComponentColumnNameSelection(
+    			new SettingsModelColumnName(NsfpNodeModel.PROPERTY_ALT_COL, NsfpNodeModel.DEFAULT_ALT_COL),
+    			"ALT", 0, true, false,
+    			new DataTypeColumnFilter(StringCell.TYPE)));
+    	
     	}
 	}
 

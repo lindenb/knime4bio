2f1d997416e8bc773a027a5cbae4336f9ac6e622
plindenbaum.u915 <plindenbaum.u915@d931a406-db19-d2ed-a82e-cc0d0b040b69>
2011-06-14 15:44:29 +0000

    cont

---
+++ fr.inserm.umr915.knime4ngs.nodes/src/fr/inserm/umr915/knime4ngs/nodes/vcf/tabix/TabixNodeFactory.java
@@ -0,0 +1,59 @@
+package fr.inserm.umr915.knime4ngs.nodes.vcf.tabix;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+
+/**
+ * <code>NodeFactory</code> for the "VCFSource" Node.
+ * Reads a VCF file
+ *
+ * @author Pierre Lindenbaum
+ */
+public class TabixNodeFactory
+        extends NodeFactory<TabixNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TabixNodeModel createNodeModel() {
+        return new TabixNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<TabixNodeModel> createNodeView(final int viewIndex,
+            final TabixNodeModel nodeModel) {
+        throw new IllegalStateException();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new TabixNodeDialog();
+    }
+
+}
+
